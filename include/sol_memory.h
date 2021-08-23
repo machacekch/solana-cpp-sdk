09e68451109f55e67a48f84cb814f95d7af589f3
Christian Machacek <39452430+machacekch@users.noreply.github.com>
2021-08-23 14:40:34 +0000

    Use the sol_memcpy_ syscall for sol::memcpy
    The syscalls for memory operations have now been enabled on
    mainnet-beta. In terms of compute units used, these are more
    efficient than performing byte-wise operations in a loop.

--- include/sol_memory.h
+++ include/sol_memory.h
@@ -3,6 +3,8 @@
  * Low-level operations on memory and addresses
  */
 
+#include <sol_syscalls.h>
+
 namespace sol {
 
 // returns the nearest address divisible by BYTES, that's larger than or equal to the argument
@@ -18,10 +20,8 @@ constexpr T* aligned(const T* ptr) {
     return reinterpret_cast<T*>(aligned<BYTES>(reinterpret_cast<uint64_t>(ptr)));
 }
 
-inline void memcpy(void* dst, const void* src, int len) {
-    for (int i = 0; i < len; ++i) {
-        *((uint8_t*)dst + i) = *((const uint8_t*)src + i);
-    }
+inline void memcpy(void* dst, const void* src, uint64_t len) {
+    syscall::sol_memcpy_(dst, src, len);
 }
 
 } // namespace sol
